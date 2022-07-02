a536bc78684ea7d82cd61ad4a1321cc78782a786
AbuSohan <sohan@inflexionpointbd.com>
2022-07-02 10:42:04 +0600

    initial commit

---
+++ functions.php
@@ -0,0 +1,78 @@
+<?php
+// My theme function
+
+// Theme title
+add_theme_support( 'title-tag' );
+
+// Theme CSS and jQuery File Calling
+function abu_css_js_file_calling(){
+    // style file
+    wp_register_style( 'bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '4.0.0', 'all' );
+    wp_register_style( 'custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all' );
+
+    wp_enqueue_style( 'abu-style', get_stylesheet_uri( ) );
+    wp_enqueue_style( 'bootstrap' );
+    wp_enqueue_style( 'custom' );
+
+
+    // jQuery
+    wp_enqueue_script( 'jquery' );
+    wp_enqueue_script( 'bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '4.0.0', 'true' );
+    wp_enqueue_script( 'main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
+};
+
+add_action( 'wp_enqueue_scripts', 'abu_css_js_file_calling' );
+
+// Google Fonts Enqueue
+function abu_add_google_fonts(){
+    wp_enqueue_style( 'abu_google_fonts', 'https://fonts.googleapis.com/css2?family=Arima&display=swap', false );
+}
+add_action( 'wp_enqueue_scripts', 'abu_add_google_fonts' );
+
+// Theme function
+function abu_customizar_register($wp_customize){
+    // Header Area Function
+    $wp_customize->add_section('abu_header_area', array(
+        'title' => __('Header Area', 'abuhorraira'),
+        'description' => 'Update Your Header Area. Example of a description'
+    ));
+
+    $wp_customize->add_setting('abu_logo', array(
+        'default' => get_bloginfo('template_directory').'/img/logo.png',
+    ));
+
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'abu_logo', array(
+        'label' => 'Logo Upload',
+        'description' => 'If you interested then change the fucking logo you dumb idiot',
+        'setting' => 'abu_logo',
+        'section' => 'abu_header_area',
+    )));
+
+    // Menu Position Option
+    $wp_customize->add_section('abu_menu_option', array(
+        'title' => __('Menu Position Option', 'abuhorraira'),
+        'description' => 'Update Your Menu Position. Example of a description.'
+    ));
+
+    $wp_customize->add_setting('abu_menu_position', array(
+        'default' => 'right_menu',
+    ));
+
+    $wp_customize->add_control('abu_menu_position', array(
+        'label' => 'Menu Position',
+        'description' => 'If you interested then change the fucking menu position you dumb idiot',
+        'setting' => 'abu_menu_position',
+        'section' => 'abu_menu_option',
+        'type' => 'radio',
+        'choices' => array(
+            'left_menu' => 'Left Menu',
+            'right_menu' => 'Right Menu',
+            'center_menu' => 'Center Menu',
+        ),
+    ));
+}
+
+add_action( 'customize_register', 'abu_customizar_register' );
+
+// Menu Register
+register_nav_menu( 'main_menu', __('Main Menu', 'abuhorraira') );
---
+++ index.php
@@ -0,0 +1,54 @@
+<?php 
+// This template for displaying the header
+?>
+
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>" class="no-js">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <div id="header_area" class="<?php echo get_theme_mod( 'abu_menu_position' ); ?>" >
+        <div class="container">
+            <div class="row">
+                <!-- <div class="col-md-3"><a href=""><img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt=""></a></div> -->
+                <div class="col-md-3"><a href=""><img src="<?php echo get_theme_mod( 'abu_logo' ); ?>" alt=""></a></div>
+            
+                <div class="col-md-9">
+                    <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
+                    <!-- <ul id="nav" >
+                        <li><a href="">Home</a></li>
+                        <li><a href="">About Us</a></li>
+                        <li><a href="">Media</a></li>
+                        <li><a href="">Download</a></li>
+                        <li><a href="">Projects</a></li>
+                        <li><a href="">Service</a>
+                        <ul>
+                            <li><a href="">Dropdown</a></li>
+                            <li><a href="">Dropdown</a></li>
+                            <li><a href="">Dropdown</a>
+                                <ul>
+                                <li><a href="">Dropdown</a></li>
+                                <li><a href="">Dropdown</a></li>
+                                <li><a href="">Dropdown</a></li>
+                                <li><a href="">Dropdown</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="">Dropdown</a></li>
+                        </ul>
+                        </li>
+                        <li><a href="">FAQ</a></li>
+                        <li><a href="">Contact Us</a></li>
+                    </ul> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    
+<?php wp_footer(); ?>
+</body>
+</html>
